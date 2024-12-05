cad606e701bb7fb7de8c8266ab9f50ebec3ec936
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-05 22:15:27 +0100

    Count nodes with one or more child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+
+/**
+  * binary_tree_nodes - Counts node with at least 1 child.
+  * @tree: Pointer to the root node to count.
+  *
+  * Return: node count or 0.
+  */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		return (1 + (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right)));
+	return (0);
+}
