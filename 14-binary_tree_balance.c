831dd8b02034e78edf8cc31d9abee941beea7e36
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-06 02:47:33 +0100

    Fix betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,15 +9,15 @@
   */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height, right_height;
+	size_t left_height, right_height;
 
-        if (!tree)
-                return (0);
-        /* Recursively get the height of left and right subtrees.*/
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
+	if (!tree)
+		return (0);
+	/* Recursively get the height of left and right subtrees.*/
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-        return ((left_height > right_height ? left_height : right_height) + 1);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 /**
   * binary_tree_balance - Measures the balance factor of a tree.
